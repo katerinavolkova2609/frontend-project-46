3fcd99d670781dd7be4713b5c0bd0de7f6abaae5
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2024-04-03 11:27:12 +0300

    add expected.file.txt

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,23 +1,31 @@
-// import { readFileSync } from "node:fs";
-// import { resolve } from "node:path";
-// import { cwd } from "node:process";
+import { readFileSync } from 'node:fs';
+import { dirname } from 'path';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
 import parseFiles from '../src/parsers.js';
 import { genDiff, stringify } from '../src/gendiff.js';
 
-const result = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getPath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getPath(filename), 'utf-8');
 
 test('genDiff, .json', () => {
+  const result = readFile('expected.stylish.txt'.trim());
   expect(
     stringify(
-      genDiff(parseFiles('__fixtures__/file1.json', '__fixtures__/file2.json')),
-    ),
+      genDiff(parseFiles('__fixtures__/file1.json', '__fixtures__/file2.json'))
+    )
   ).toEqual(result);
 });
 
 test('genDiff, .yaml', () => {
+   const result = readFile('expected.stylish.txt'.trim());
   expect(
     stringify(
-      genDiff(parseFiles('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')),
-    ),
+      genDiff(parseFiles('__fixtures__/file1.yaml', '__fixtures__/file2.yaml'))
+    )
   ).toEqual(result);
 });
