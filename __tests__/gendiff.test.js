e3af32ebc7429f1e7e83bb6205a82b483c103924
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2024-04-11 10:44:11 +0300

    edit tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,43 +11,19 @@ const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filenam
 const readFile = (filename) => readFileSync(getPath(filename), 'utf-8');
 
 const stylishData = readFile('expected.stylish.txt'.trim());
-const stylishDataYaml = readFile('expected.stylish.txt'.trim());
 const plainData = readFile('expected.stylish.plain.txt'.trim());
-const plainDataYaml = readFile('expected.stylish.plain.txt'.trim());
 const jsonFormat = readFile('expected.stylish.json.txt'.trim());
 
-test('genDiff, .json', () => {
-  expect(
-    genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish'),
-  ).toEqual(stylishData);
-});
-
-test('genDiff, .yaml', () => {
-  expect(
-    genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'stylish'),
-  ).toEqual(stylishDataYaml);
-});
-
-test('genDiff, .json plain', () => {
-  expect(
-    genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain'),
-  ).toEqual(plainData);
-});
-
-test('genDiff, .yaml plain', () => {
-  expect(
-    genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'plain'),
-  ).toEqual(plainDataYaml);
-});
-
-test('genDiff, .json JSON', () => {
-  expect(
-    genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json'),
-  ).toEqual(jsonFormat);
-});
-
-test('genDiff, .yml JSON', () => {
-  expect(
-    genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json'),
-  ).toEqual(jsonFormat);
+test.each([
+  [stylishData, '__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish'],
+  [stylishData, '__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'stylish'],
+  [stylishData, '__fixtures__/file1.yml', '__fixtures__/file2.yml', 'stylish'],
+  [plainData, '__fixtures__/file1.json', '__fixtures__/file2.json', 'plain'],
+  [plainData, '__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'plain'],
+  [plainData, '__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain'],
+  [jsonFormat, '__fixtures__/file1.json', '__fixtures__/file2.json', 'json'],
+  [jsonFormat, '__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json'],
+  [jsonFormat, '__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'json'],
+])('%# comparing with the reference sample', (expected, filepath1, filepath2, formatStyle) => {
+  expect(genDiff(filepath1, filepath2, formatStyle)).toEqual(expected);
 });
